8885105c56fb5a73537db62ccd6219a8a11550b6
carlosdevgrx <holacarlosfernandez@gmail.com>
2026-04-09 15:59:04 +0200

    fix: usar componente Analytics de @vercel/analytics en lugar de inject()

--- stridely/src/main.tsx
+++ stridely/src/main.tsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { inject } from '@vercel/analytics'
+import { Analytics } from '@vercel/analytics/react'
 import './styles/globals.scss'
 import App from './App.tsx'
-
-inject()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
 
